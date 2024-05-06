01fb6a87fe21c8a10f133dc99b49258092131b02
Arthur melo <arthur.ian@aluno.uepb.edu.br>
2024-05-06 23:57:55 +0000

    feat: added reactt-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App />, errorElement: <h1>OOOPSS! This page does not exist (ERROR 404)</h1> },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
